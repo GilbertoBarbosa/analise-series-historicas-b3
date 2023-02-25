6c76803d0f775c1f7135bd79412af7a02ee63bdf
= <=>
2023-02-25 11:53:42 -0300

    Update

--- analise_acoes.R
+++ analise_acoes.R
@@ -41,8 +41,6 @@ summary(df$PREMAX)
 # Empresas
 table(df$CODNEG)
 
-head(df)
-
 
 # Codigo de Negociação do papel (CODNEG)
 
@@ -51,16 +49,40 @@ COD = 'ELET6'
 dfCODNEG <- filter(df, trim(CODNEG) == COD)
 
 
-# Gera gráfico de linhas para Preço de Abertura do Papelmercado no pregão
+# Gera gráfico de linhas para Preço de Abertura
 titulo = paste("Evolução do Preço de Abertura. Código:", COD, sep = ' ')
 cor="blue"
 gerar_grafico(dfCODNEG, dfCODNEG$DATAP, dfCODNEG$PREABE, titulo, cor)
 
-# Gera gráfico de linhas para Preço de Abertura do Papelmercado no pregão
+# Gera gráfico de linhas para Preço Máximo
 titulo = paste("Evolução do Preço Máximo. Código:", COD, sep = ' ')
 cor="green"
-  gerar_grafico(dfCODNEG, dfCODNEG$DATAP, dfCODNEG$PREMAX, titulo, cor)
+gerar_grafico(dfCODNEG, dfCODNEG$DATAP, dfCODNEG$PREMAX, titulo, cor)
 
+# Gera gráfico de linhas para Preço Mínimo
+titulo = paste("Evolução do Preço Mínimo Código:", COD, sep = ' ')
+cor="orange"
+gerar_grafico(dfCODNEG, dfCODNEG$DATAP, dfCODNEG$PREMIN, titulo, cor)
+
+# Gera gráfico de linhas para Preço Médio
+titulo = paste("Evolução do Preço Médio Código:", COD, sep = ' ')
+cor="brown"
+gerar_grafico(dfCODNEG, dfCODNEG$DATAP, dfCODNEG$PREMED, titulo, cor)
+
+# Gera gráfico de linhas para Preço do último negócio
+titulo = paste("Evolução do Preço do último negócio Código:", COD, sep = ' ')
+cor="red"
+gerar_grafico(dfCODNEG, dfCODNEG$DATAP, dfCODNEG$PREULT, titulo, cor)
+
+# Gera gráfico de linhas para Melhor oferta de compra
+titulo = paste("Evolução do Preço da Melhor Oferta de Compra Código:", COD, sep = ' ')
+cor="gray"
+gerar_grafico(dfCODNEG, dfCODNEG$DATAP, dfCODNEG$PREOFC, titulo, cor)
+  
+# Gera gráfico de linhas para Melhor oferta de venda
+titulo = paste("Evolução do Preço da Melhor Oferta de Venda Código:", COD, sep = ' ')
+cor="black"
+gerar_grafico(dfCODNEG, dfCODNEG$DATAP, dfCODNEG$PREOFV, titulo, cor)
 
 # Libera memória
 unlink(df)
---
+++ decomposicao_serie.R
@@ -0,0 +1,56 @@
+
+# Carrega bibliotecas necessárias
+library(dplyr)
+library(gdata)
+library(ggplot2)
+
+# Aponta para o diretório onde estão os arquivos
+setwd("~/analise-series-historicas-b3")
+
+# Carrega funções que serão utilizadas no script
+source("funcoes.R")
+
+
+# Realiza o carregamento do arquivo de texto
+# Os arquivos poderão ser acessados pelo link oficial da B3:
+# https://www.b3.com.br/pt_br/market-data-e-indices/servicos-de-dados/market-data/historico/mercado-a-vista/series-historicas/
+
+df <- ler_arquivo("COTAHIST_A2022.TXT")
+
+# Faz a leitura das primeiras linhas do arquivo
+head(df)
+
+# Converte string para Data
+df$DATAP = as.Date(df$DATAP, "%Y%m%d")
+
+# Obtém as casas decimais dos valores
+df$PREABE = df$PREABE/100
+df$PREMAX = df$PREMAX/100
+df$PREMIN = df$PREMIN/100
+df$PREMED = df$PREMED/100
+df$PREULT = df$PREULT/100
+df$PREOFC = df$PREOFC/100
+df$PREOFV = df$PREOFV/100
+df$VOLTOT = df$VOLTOT/100
+df$PREEXE = df$PREEXE/100
+
+
+# Resumo estatístico
+summary(df$PREMAX)
+
+# Empresas
+table(df$CODNEG)
+
+
+# Codigo de Negociação do papel (CODNEG)
+
+COD = 'ELET6'
+
+dfCODNEG <- filter(df, trim(CODNEG) == COD)
+
+x <- ts(dfCODNEG$PREULT, 1, 20, 12)
+
+plot(stl(x, s.window = "per"))
+
+# Libera memória
+unlink(df)
